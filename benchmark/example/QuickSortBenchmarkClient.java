7585ecf3c424c1c2af6e69b2b69c36e46983d724
Enilesi <miriam.iseline@gmail.com>
2025-05-10 22:28:25 +0300

    Finished milestone 5

---
+++ benchmark/example/QuickSortBenchmarkClient.java
@@ -0,0 +1,26 @@
+package benchmark.example;
+
+import benchmark.IBenchmark;
+import logging.ConsoleLogger;
+import logging.ILogger;
+import timing.ITimer;
+import timing.Timer;
+
+import java.util.concurrent.TimeUnit;
+
+public class QuickSortBenchmarkClient {
+    public static void main(String[] args) {
+        ITimer timer = new Timer();
+        ILogger log = new ConsoleLogger();
+        IBenchmark bench = new QuickSortBenchmark();
+
+        bench.init(10000);
+        timer.start();
+        bench.run();
+        long time = timer.stop();
+
+        log.writeTime("Finished in", time, TimeUnit.NANOSECONDS);
+        log.close();
+        bench.clean();
+    }
+}
